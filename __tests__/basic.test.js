8cf5d60f11e73d52a97a5f0e8f49a4b9693640a8
afrid <afridahamed14@gmail.com>
2024-02-06 09:22:07 +0530

    First commit

---
+++ __tests__/basic.test.js
@@ -0,0 +1,46 @@
+import { afterAll, beforeAll, describe, expect, test } from "vitest";
+import { preview } from "vite";
+import { PreviewServer } from "vite";
+import puppeteer from "puppeteer";
+import { Browser, Page } from "puppeteer";
+
+describe("basic", async () => {
+  let server = PreviewServer;
+  let browser = Browser;
+  let page = Page;
+
+  beforeAll(async () => {
+    server = await preview({ server: { port: 3000 }, preview: { port: 3000 } });
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 1000,
+    });
+    page = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+    await new Promise((resolve, reject) => {
+      server.httpServer.close((error) => (error ? reject(error) : resolve()));
+    });
+  });
+
+  test("should have the correct title", async () => {
+    try {
+      await page.goto("http://localhost:3000");
+      const button = await page.$("#btn");
+      expect(button).toBeDefined();
+
+      let text = await page.evaluate((btn) => btn.textContent, button);
+      expect(text).toBe("Clicked 0 time(s)");
+
+      await button.click();
+      text = await page.evaluate((btn) => btn.textContent, button);
+      expect(text).toBe("Clicked 1 time(s)");
+    } catch (e) {
+      console.error(e);
+      console.error(typeof e); // Log the type of the error
+      expect(e).toBeUndefined(); // Line causes error
+    }
+  }, 60_000);
+});
