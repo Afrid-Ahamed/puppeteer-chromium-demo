ea7a3918214fe1cc65fd3394319ed312f6131ce3
afrid <afridahamed14@gmail.com>
2024-02-06 23:17:00 +0530

    Removed beforeAll and try-catch

--- __tests__/basic.test.js
+++ __tests__/basic.test.js
@@ -9,14 +9,23 @@ describe("basic", async () => {
   let browser = Browser;
   let page = Page;
 
-  beforeAll(async () => {
-    server = await preview({ server: { port: 3000 }, preview: { port: 3000 } });
-    browser = await puppeteer.launch({
-      headless: false,
-      slowMo: 1000,
-    });
-    page = await browser.newPage();
+  // beforeAll(async () => {
+  //   server = await preview({ server: { port: 3000 }, preview: { port: 3000 } });
+  //   browser = await puppeteer.launch({
+  //     headless: false,
+  //     slowMo: 1000,
+  //   });
+  //   page = await browser.newPage();
+  // });
+
+  // (async () => {
+  server = await preview({ server: { port: 3000 }, preview: { port: 3000 } });
+  browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 1000,
   });
+  page = await browser.newPage();
+  // })();
 
   afterAll(async () => {
     await browser.close();
@@ -25,21 +34,21 @@ describe("basic", async () => {
     });
   });
 
-  test("should have the correct title", async () => {
-    try {
-      await page.goto("http://localhost:3000");
-      const button = await page.$("#btn");
-      expect(button).toBeDefined();
+  test("Button click should display the correct text", async () => {
+    // try {
+    await page.goto("http://localhost:3000");
+    const button = await page.$("#btn");
+    expect(button).toBeDefined();
 
-      let text = await page.evaluate((btn) => btn.textContent, button);
-      expect(text).toBe("Clicked 0 time(s)");
+    let text = await page.evaluate((btn) => btn.textContent, button);
+    expect(text).toBe("Clicked 0 time(s)");
 
-      await button.click();
-      text = await page.evaluate((btn) => btn.textContent, button);
-      expect(text).toBe("Clicked 1 time(s)");
-    } catch (e) {
-      console.error(e);
-      expect(e).toBeUndefined(); // Line causes error, if commented, tests pass though I'm unable to see the execution
-    }
+    await button.click();
+    text = await page.evaluate((btn) => btn.textContent, button);
+    expect(text).toBe("Clicked 1 time(s)");
+    // } catch (e) {
+    //   console.error(e);
+    //   expect(e).toBeUndefined(); // Line causes error, if commented, tests pass though I'm unable to see the execution
+    // }
   }, 60_000);
 });
